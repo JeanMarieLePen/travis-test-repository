e06c4f2036a560a1b56b41bcca6ee962600982c0
Aleksandar <mozartkaraokeversion@gmail.com>
2022-10-25 21:43:04 +0200

    dodati testovi za test kontroler

--- travis-test-projekat/src/test/java/com/example/demo/DemoApplicationTests.java
+++ travis-test-projekat/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,48 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class DemoApplicationTests {
+	
+	
+	@Autowired
+	private WebApplicationContext webApplicationContext;
 
-	@Test
-	void contextLoads() {
+	
+	private MockMvc mockMvc;
+	
+	@BeforeEach
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 	}
-
+	
+	
+	
+	@Test
+	public void test() {
+		assertEquals(1, 1);
+	}
+	
+	@Test
+	public void testWelcone() throws Exception {
+		MvcResult res = mockMvc.perform(get("/hello")).andExpect(status().isOk()).andReturn();
+	}
 }
